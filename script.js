dce12c58238464c2c2979dd70358059a15149f82
martnx <martinmart2002@gmail.com>
2024-07-09 02:47:50 +0800

    Finished the getHumanChoice and getComputerChoice function

--- script.js
+++ script.js
@@ -6,21 +6,41 @@ const computerScore = 0;
 
 function getHumanChoice(){
     const input = parseInt(prompt("1 = Rock | 2 = Paper | 3 = Scissors? "));
-    switch(input){
-        case 1: return input; break;
-        case 2: return input; break;
-        case 3: return input; break;
-        default: console.log("Choose 1-3 only!"),getHumanChoice();
+    if(input > 3){
+        console.log("Choose 1-3 only!");
+        getHumanChoice();
+    }
+    else if(input === 1){
+        return input;
+    }
+    else if(input === 2){
+        return input;
+    }
+    else if(input === 3){
+        return input;
+    }
+    else{
+        return;
     }
 }
 
 function getComputerChoice(){
-
     const choice = Math.floor(Math.random() * 3)+1;
     //planning to use switch statement here
     return choice;
-
 }
 
-console.log(getHumanChoice());
-console.log("computer choice: "+getComputerChoice());
+function playRound(humanChoice, computerChoice){
+    
+    let humanChoice = humanChoice;
+    let computerChoice = computerChoice;
+
+    //if else statement do it :0
+}
+
+let x = getHumanChoice();
+let y = getComputerChoice();
+
+console.log("X = " + x + " Y = " + y);
+
+playRound(x,y);
